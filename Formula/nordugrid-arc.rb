d2b7be0219219ed4542c038f36fbe8c1654548fb
Grega Vrbančič <grega.vrbancic@gmail.com>
2020-08-02 14:53:22 +0200

    Fix sha256 update

--- Formula/nordugrid-arc.rb
+++ Formula/nordugrid-arc.rb
@@ -3,7 +3,7 @@ class NordugridArc < Formula
   homepage "http://www.nordugrid.org/arc/arc6/"
   version "6.7.0"
   url "https://download.nordugrid.org/packages/nordugrid-arc/releases/#{version}/src/nordugrid-arc-#{version}.tar.gz"
-  sha256 "93a5af2e1e5e767bd17fa0b49ed1f446891e5e7b97b041b0caf523a3cc0633bd"
+  sha256 "156265ff7dcebe94e61e850ce11584ad70ee25f885bba9277810d8ed2e1ab408"
 
   depends_on "pkg-config" => :build
   depends_on "gettext"
